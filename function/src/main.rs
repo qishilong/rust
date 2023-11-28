f513d04b3e8ebb46d8ab6c9e24dc9a901fd1e94c
qishilong <663253062@qq.com>
2023-11-28 22:05:14 +0800

    ci(function): 函数
    ci(function): 函数

---
+++ function/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // println!("Hello, world!");
+    // print_value(10, 'a');
+
+    // let y = {
+    //     let x = 1;
+    //     x + 1
+    // };
+    //
+    // println!("The value y of {y}");
+
+    // let number = get_number();
+    //
+    // println!("The value of number is {number}");
+
+    // let num = get_number(1);
+    // println!("The value of number is {num}")
+}
+
+// fn print_value(value: i32, label: char) {
+//     println!("The value is {value} and {label}")
+// }
+
+// fn get_number() -> i32 {
+//     1
+// }
+
+// fn get_number(x: i32) -> i32 {
+//     x + 1
+// }
