c1d196b5bb785d4aa75c34dcd85a8bd9af9020cd
qishilong <663253062@qq.com>
2023-11-28 21:23:12 +0800

    ci(number_calc): 数值运算
    ci(number_calc): 数值运算

---
+++ number_calc/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    let sum = 0.1 + 0.2;
+    let diff = 0.3 - 0.1;
+    let multiplication = 10 * 10;
+    let division = 5 / 2;
+    let mod_number = 100 % 3;
+
+    println!(
+        "{}\n{}\n{}\n{}\n{}",
+        sum, diff, multiplication, division, mod_number
+    )
+}
